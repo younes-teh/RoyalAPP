6c9546a77399964419a7507035341e619d45854f
younes-teh <younes.boualam66@gmail.com>
2026-06-02 15:16:53 +0100

    Add project files.

---
+++ RoyalVilla.DTO/VillaUpdateDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoyalVilla.DTO
+{
+    public class VillaUpdateDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public required string Name { get; set; }
+        public string? Details { get; set; }
+        public double Rate { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string? ImageUrl { get; set; }
+        
+    }
+}
---
+++ RoyalVilla_API/Services/AuthService.cs
@@ -0,0 +1,112 @@
+﻿using AutoMapper;
+using Azure.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using RoyalVilla_API.Data;
+using RoyalVilla_API.Models;
+using RoyalVilla.DTO;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace RoyalVilla_API.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
+
+        public AuthService(ApplicationDbContext db, IConfiguration configuration, IMapper mapper)
+        {
+            _db = db;
+            _configuration = configuration;
+            _mapper = mapper;
+        }
+
+
+        public async Task<bool> IsEmailExistsAsync(string email)
+        {
+            return await _db.Users.AnyAsync(u => u.Email.ToLower() == email.ToLower());
+        }
+
+        public async Task<LoginResponseDTO?> LoginAsync(LoginRequestDTO loginRequestDTO)
+        {
+            try
+            {
+
+                var user = await _db.Users.FirstOrDefaultAsync(u => u.Email.ToLower() == loginRequestDTO.Email.ToLower());
+
+                if (user == null || user.Password != loginRequestDTO.Password)
+                {
+                    return null;
+                }
+
+                //generate TOKEN
+                var token = GenerateJwtToken(user);
+                return new LoginResponseDTO
+                {
+                    UserDTO = _mapper.Map<UserDTO>(user),
+                    Token = token
+                };
+            }
+            catch (Exception ex)
+            {
+                // Handle any other unexpected errors
+                throw new InvalidOperationException("An unexpected error occurred during user login", ex);
+            }
+        }
+
+        public async Task<UserDTO?> RegisterAsync(RegisterationRequestDTO registerationRequestDTO)
+        {
+            try
+            {
+                if (await IsEmailExistsAsync(registerationRequestDTO.Email))
+                {
+                    throw new InvalidOperationException($"User with email '{registerationRequestDTO.Email}' already exists");
+                }
+
+                User user = new()
+                {
+                    Email = registerationRequestDTO.Email,
+                    Name = registerationRequestDTO.Name,
+                    Password = registerationRequestDTO.Password,
+                    Role = string.IsNullOrEmpty(registerationRequestDTO.Role) ? "Customer" : registerationRequestDTO.Role,
+                    CreatedDate = DateTime.Now
+                };
+
+                await _db.Users.AddAsync(user);
+                await _db.SaveChangesAsync();
+
+                return _mapper.Map<UserDTO>(user);
+            }
+            catch (Exception ex)
+            {
+                // Handle any other unexpected errors
+                throw new InvalidOperationException("An unexpected error occurred during user registration", ex);
+            }
+        }
+
+
+        private string GenerateJwtToken(User user)
+        {
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("JwtSettings")["Secret"]);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+                    new Claim(ClaimTypes.Email,user.Email),
+                    new Claim(ClaimTypes.Name,user.Name),
+                    new Claim(ClaimTypes.Role,user.Role),
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
